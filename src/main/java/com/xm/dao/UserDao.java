ee6cd01c10860f6300c6a80e899912e9e6cfaced
admin <651307866@qq.com>
2018-04-10 15:42:16 +0800

    ssm项目测试

---
+++ src/main/java/com/xm/dao/UserDao.java
@@ -0,0 +1,19 @@
+package com.xm.dao;
+
+import com.xm.pojo.User;
+
+public interface UserDao {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+    
+    User findByUsername(String username);
+}
